64175c3982442ce7f54d4712575a9f4adb76d11a
Alina Yakimova <komarovatz@gmail.com>
2021-03-25 22:14:37 +0300

    доделана 3 неделя, первый отчет

--- fullscreen-menu.js
+++ fullscreen-menu.js
@@ -5,10 +5,20 @@ let menu = document.querySelector(".fullscreen-menu");
 let body = document.querySelector("body");
 
 let toggleMenu = function() {
-  button.classList.toggle(".is-active");
-  menu.classList.toggle(".fullscreen-menu--active");
+  button.classList.toggle("is-active");
+  menu.classList.toggle("fullscreen-menu--active");
   body.classList.toggle("body-active-menu");
+
 }
 
 button.addEventListener("click", toggleMenu)
+// меню.отслеживатьСобытия("клик", функция(событие))
+menu.addEventListener("click", function (event) {
+  // if(событие.цель.список классов.содержит("меню линк")) {
+if(event.target.classList.contains("menu__link")){
+  toggleMenu()
+  }
+  // console.log(event.target.classList.contains("menu__link"));
+  // console.log(event.target)
 });
+})()
