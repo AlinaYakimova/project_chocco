c4dd8b42d765700b73b6312bc49494621991189b
Alina Yakimova <komarovatz@gmail.com>
2021-04-05 22:11:39 +0300

    проблемы с OPS

--- ops.js
+++ ops.js
@@ -1,44 +1,44 @@
 const sections = $("section");
 const display = $(".main-content");
 
-// sections.first().addClass("active");
+sections.first().addClass("active");
 
-// const performTransition = sectionEq => {
-//   const position = sectionEq * -100;
+const performTransition = sectionEq => {
+  const position = sectionEq * -100;
 
-//   display.css({
-//     transform: `translateY(${position}%)`,
-//   })
+  display.css({
+    transform: `translateY(${position}%)`,
+  })
 
-//   sections.eq(sectionEq).addClass("active").siblings().removeClass("active");
-// }
+  sections.eq(sectionEq).addClass("active").siblings().removeClass("active");
+}
 
-// const scrollViewport = direction => {
-//   const activeSection = sections.filter(".active");
-//   const nextSection = activeSection.next();
-//   const prevSection = activeSection.prev();
+const scrollViewport = direction => {
+  const activeSection = sections.filter(".active");
+  const nextSection = activeSection.next();
+  const prevSection = activeSection.prev();
 
-//   if (direction === "next") {
-//     performTransition(nextSection.index())
-//   }
+  if (direction === "next") {
+    performTransition(nextSection.index())
+  }
 
-//   if (direction === "prev") {
-//     performTransition(prevSection.index())
-//   }
-// }
+  if (direction === "prev") {
+    performTransition(prevSection.index())
+  }
+}
 
 $(window).on("wheel", e => {
   const deltaY = e.originalEvent.deltaY;
 
   console.log(deltaY);
 
-  // if (deltaY > 0) {
-  //   // next
-  //   scrollViewport("next");
-  // }
+  if (deltaY > 0) {
+    // next
+    scrollViewport("next");
+  }
 
-  // if (deltaY < 0) {
-  //   // prev
-  // }
-  // scrollViewport("prev");
+  if (deltaY < 0) {
+    // prev
+  }
+  scrollViewport("prev");
 });
